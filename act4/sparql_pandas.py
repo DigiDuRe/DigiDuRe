0eb9bb8df887f6ff016247c2bef173302e2f3ed6
Maurice de Kleijn <mtm.de.kleijn@gmail.com>
2023-07-25 18:40:29 +0200

    modularized sparql part

---
+++ act4/sparql_pandas.py
@@ -0,0 +1,18 @@
+def sparql_to_dataframe(sparql_endpoint, sparql_query):
+    sparql = SPARQLWrapper(sparql_endpoint)
+    sparql.setQuery(sparql_query)
+    sparql.setReturnFormat(JSON)
+
+    results = sparql.query().convert()
+
+    bindings = results["results"]["bindings"]
+    data = []
+
+    for item in bindings:
+        row = {}
+        for key in item:
+            row[key] = item[key]["value"]
+        data.append(row)
+
+    df = pd.DataFrame(data)
+    return df
